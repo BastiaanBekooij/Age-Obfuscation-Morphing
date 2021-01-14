c20792e682c7eb99d6a87729351813e558f051b9
muriel1 <murielspek@gmail.com>
2021-01-14 16:05:10 +0100

    ROC werkend

--- code/create_ROC.py
+++ code/create_ROC.py
@@ -11,74 +11,77 @@ import numpy as np
 
 f = open('C:/Users/murie/Documents/school/master/1B/Introduction2Biometrics/project_git/data/before_rec_csv', 'r')
 
-TP = []
-FP = []
-FN = []
-TN = []
+#TP = []
+#FP = []
+#FN = []
+#TN = []
 FPR = []
 TPR = []
 
-#TP =0
-#FP = 0
-#FN = 0
-#TN = 0
+TP =0
+FP = 0
+FN = 0
+TN = 0
 #FPR = 0
 #TPR = 0
 
-th=0.5
+#th=0.5
+th_range = []
 
-for i in f:
-    #print(i)
-    data = i.split(';')
-    #print(data[0])
-    #other.append(float(data[0]))
-    #print(float(data[0]))
-    #print(float(data[1]))
-    if float(data[0]) > th:
-        FN.append(float(data[0]))
-    if float(data[1]) > th:
-        TN.append(float(data[1]))
-        #print(len(TN))
-    if float(data[0]) < th:
-        TP.append(float(data[0]))
-        #print(TP)
-    if float(data[1]) < th:
-        FP.append(float(data[1]))
-    #dist2otherE.append(float(data[2]))
-    f.close
-    
-    
-    
-print("TP: ", len(TP))
-print("FP: ", len(FP))  
-print("TN: ", len(TN))  
-print("FN: ", len(FN))    
-#skplt.metrics.plot_roc_curve(y_true, y_probas)
+for th in np.arange(0, 1, 0.0001):
+    #print("th: ", th)
+    if (FP+TN) != 0:
+        FPR.append(FP / (FP+TN))
+        TPR.append(TP / (TP+FN))
+        #print("FPR: ", FPR)
+        #print("TPR: ", TPR)
+        print("TP: ", TP)
+        print("FP: ", FP)  
+        print("TN: ", TN)  
+        print("FN: ", FN)
+        
+    TP = 0
+    FP = 0
+    FN = 0
+    TN = 0
+    f = open('C:/Users/murie/Documents/school/master/1B/Introduction2Biometrics/project_git/data/before_rec_csv', 'r')
 
-for i in range(len(FP+TN)):
-    FPR.append(sum(FP[0:i]) / len(FP+TN))
+    for i in f:
+        #print("th: ", th)
+        data = i.split(';')
+        if float(data[0]) > th:
+            #FN.append(float(data[0]))
+            FN = FN+1
+        if float(data[1]) > th:
+            #TN.append(float(data[1]))
+            #print(len(TN))
+            TN = TN+1
+        if float(data[0]) < th:
+            #TP.append(float(data[0]))
+            TP = TP+1
+        if float(data[1]) < th:
+            #FP.append(float(data[1]))
+            FP = FP+1
+        f.close
+        
+    
+#print("TP: ", len(TP))
+#print("FP: ", len(FP))  
+#print("TN: ", len(TN))  
+#print("FN: ", len(FN))    
+
+print("TP: ", TP)
+print("FP: ", FP)  
+print("TN: ", TN)  
+print("FN: ", FN)
+
+#for i in range(len(FP)+len(TN)):
+#    FPR.append(sum(FP[0:i]) / len(FP+TN)) # does not change
     #FPR.append(sum(FP[0:i]) / len(FP+TN))
     #print(FPR)
-for j in range(len(TP+FN)):
-    TPR.append(sum(FP[0:i]) / len(TP+FN))   
-    
-print(np.shape(TPR))
-print(np.shape(FPR))
-print(len(TPR))
-print(len(FPR))
-            
-#TPR = np.array(TPR)
-#TPR = np.expand_dims(a, axis=-1) # Add an extra dimension in the last axis.
-#FPR = np.array(FPR)
-    
-#fpr, tpr, threshold = metrics.roc_curve(y_test, preds)
+#for j in range(len(TP)+len(FN)):
+#    TPR.append(sum(TP[0:j]) / len(TP+FN)) 
 
-#FPR = float(len(FP) / len(FP+TN)) # 0,5
-#TPR = float(len(TP) / len(TP+FN)) # 1
-#print(TPR)
-
-#x = FPR # false_positive_rate
-#y = TPR # true_positive_rate 
 
 # This is the ROC curve
 plt.plot(FPR, TPR)
