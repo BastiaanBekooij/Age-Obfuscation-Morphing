9d5859d1e8821b0225ecaadb44db8a84d7836537
muriel1 <murielspek@gmail.com>
2021-01-14 16:57:34 +0100

    ROC en DET curve

--- code/create_ROC.py
+++ code/create_ROC.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 #import scikitplot as skplt
 
-f = open('C:/Users/murie/Documents/school/master/1B/Introduction2Biometrics/project_git/data/before_rec_csv', 'r')
+#f = open('C:/Users/murie/Documents/school/master/1B/Introduction2Biometrics/project_git/data/before_rec_csv', 'r')
 
 #TP = []
 #FP = []
@@ -17,6 +17,7 @@ f = open('C:/Users/murie/Documents/school/master/1B/Introduction2Biometrics/proj
 #TN = []
 FPR = []
 TPR = []
+FNMR = []
 
 TP =0
 FP = 0
@@ -26,65 +27,78 @@ TN = 0
 #TPR = 0
 
 #th=0.5
-th_range = []
+th_range = np.arange(0, 1, 0.01)
 
-for th in np.arange(0, 1, 0.0001):
+for th in th_range:
     #print("th: ", th)
     if (FP+TN) != 0:
         FPR.append(FP / (FP+TN))
         TPR.append(TP / (TP+FN))
-        #print("FPR: ", FPR)
-        #print("TPR: ", TPR)
+        FNMR.append(1-TP / (TP+FN))
+        #print("FPR: ", FP / (FP+TN))
+        #print("TPR: ", TP / (TP+FN))
         print("TP: ", TP)
-        print("FP: ", FP)  
-        print("TN: ", TN)  
-        print("FN: ", FN)
-        
+        #print("FP: ", FP)  
+        #print("TN: ", TN)  
+        #print("FN: ", FN)
+    
     TP = 0
     FP = 0
     FN = 0
     TN = 0
-    f = open('C:/Users/murie/Documents/school/master/1B/Introduction2Biometrics/project_git/data/before_rec_csv', 'r')
+    f = open('C:/Users/murie/Documents/school/master/1B/Introduction2Biometrics/project_git/data/before_rec_ADJUSTED_csv', 'r')
 
     for i in f:
-        #print("th: ", th)
         data = i.split(';')
+        #print("data: ", data[0])
         if float(data[0]) > th:
-            #FN.append(float(data[0]))
             FN = FN+1
         if float(data[1]) > th:
-            #TN.append(float(data[1]))
-            #print(len(TN))
             TN = TN+1
         if float(data[0]) < th:
-            #TP.append(float(data[0]))
             TP = TP+1
         if float(data[1]) < th:
-            #FP.append(float(data[1]))
             FP = FP+1
         f.close
         
-    
-#print("TP: ", len(TP))
-#print("FP: ", len(FP))  
-#print("TN: ", len(TN))  
-#print("FN: ", len(FN))    
 
-print("TP: ", TP)
-print("FP: ", FP)  
-print("TN: ", TN)  
-print("FN: ", FN)
-
-#for i in range(len(FP)+len(TN)):
-#    FPR.append(sum(FP[0:i]) / len(FP+TN)) # does not change
-    #FPR.append(sum(FP[0:i]) / len(FP+TN))
-    #print(FPR)
-#for j in range(len(TP)+len(FN)):
-#    TPR.append(sum(TP[0:j]) / len(TP+FN)) 
+#print("TP: ", TP)
+#print("FP: ", FP)  
+#print("TN: ", TN)  
+#print("FN: ", FN)
 
 
-# This is the ROC curve
-plt.plot(FPR, TPR)
+# Plot FPR
+plt.plot(th_range[0:len(FPR)], FPR)
+plt.xlabel('Threshold')
+plt.ylabel('False Positive Rate')
+plt.title('FPR')
+plt.show()
+
+# Plot TPR
+plt.plot(th_range[0:len(TPR)], TPR)
+plt.xlabel('Threshold')
+plt.ylabel('True Positive Rate')
+plt.title('TPR')
+plt.show()
+
+# Plot ROC
+plt.plot(FPR, TPR, color='orange', label='ROC')
+plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC) Curve')
+plt.legend()
 plt.show() 
+
+# Plot DET
+plt.plot(FPR, FNMR, color='orange', label='DET')
+plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
+plt.xlabel('False Positive Rate')
+plt.ylabel('False non-Match Rate')
+plt.title('Decision Error Trade-off (DET) curve')
+plt.legend()
+plt.show() 
+
 
 
